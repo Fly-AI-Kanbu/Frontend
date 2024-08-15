1959377ab3efed1c11417ce43b7d8c1b4f78302b
JeongHunOfficial <n_junghoon@naver.com>
2024-08-15 16:07:09 +0900

    feat: Header 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { LoadingPage } from "./page/Loading/LodingPage";
-import Header from "./components/Header";
 import { MainPage } from "./page/Main/MainPage";
 
 export const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Header />
+        {/*<Header />*/}
         <Routes>
           <Route path="/" element={<MainPage />} />
         </Routes>
--- src/page/Main/MainPage.jsx
+++ src/page/Main/MainPage.jsx
@@ -1,6 +1,5 @@
 import { css } from '@emotion/css';
 
-import HeaderDummy from "@components/HeaderDummy";
 import { RankComponent } from './RankComponent';
 import { NewsComponent } from './NewsComponent';
 import { TodayVocabComponent } from './TodayVocabComponent';
@@ -10,6 +9,7 @@ const mainPageStyle = css`
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding-top: 3em;
 
   gap: 1.5em;
   width: 100vw;
@@ -18,7 +18,6 @@ const mainPageStyle = css`
 export const MainPage = () => {
   return (
     <div className={mainPageStyle}>
-      <HeaderDummy />
       <RankComponent />
       <TodayVocabComponent />
       <VisitDaysComponent />
