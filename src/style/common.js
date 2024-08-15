bae411d27f84c20509e9d7036b5c643a6074b0f5
JeongHunOfficial <n_junghoon@naver.com>
2024-08-15 16:28:41 +0900

    style: Color Palette 설정

--- src/style/common.js
+++ src/style/common.js
@@ -1,11 +1,61 @@
 const Common = {
   colors: {
-    primary: '#0F4C82',
-    primaryDark: '#004664',
-    primaryLight: '#1dc9dc',
-    secondary: '#582c7f',
-    text: '#ffffff',
-    gray: '#e0e0e0',
+    background: '#eee',
+    text: '#222',
+    border: '#ddd',
+
+    primary100: '#040D12',
+    primary200: '#0A2129',
+    primary300: '#183D3D',
+    primary400: '#2a4b4a',
+    primary500: '#43605f',
+    primary600: '#5C8374',
+    primary700: '#7A9F8A',
+    primary800: '#8FAE9A',
+    primary900: '#93B1A6',
+
+    white: '#ffffff',
+    black: '#000000',
+
+    gray100: '#f5f5f5',
+    gray200: '#eeeeee',
+    gray300: '#e0e0e0',
+    gray400: '#bdbdbd',
+    gray500: '#9e9e9e',
+    gray600: '#757575',
+    gray700: '#616161',
+    gray800: '#424242',
+    gray900: '#212121',
+
+    red100: '#FFEBEE',
+    red200: '#FFCDD2',
+    red300: '#EF9A9A',
+    red400: '#E57373',
+    red500: '#EF5350',
+    red600: '#E53935',
+    red700: '#D32F2F',
+    red800: '#C62828',
+    red900: '#B71C1C',
+
+    blue100: '#E3F2FD',
+    blue200: '#BBDEFB',
+    blue300: '#90CAF9',
+    blue400: '#64B5F6',
+    blue500: '#42A5F5',
+    blue600: '#2196F3',
+    blue700: '#1E88E5',
+    blue800: '#1976D2',
+    blue900: '#1565C0',
+
+    green100: '#E8F5E9',
+    green200: '#C8E6C9',
+    green300: '#A5D6A7',
+    green400: '#81C784',
+    green500: '#66BB6A',
+    green600: '#4CAF50',
+    green700: '#43A047',
+    green800: '#388E3C',
+    green900: '#2E7D32'
   }
 }
 
